d6b723ce30cd073f7f5cc9547cd9d08816ea64b7
linxiaoping <285624489@qq.com>
2020-08-31 14:55:10 +0800

    add NSBundle category

---
+++ Example/Pods/Target Support Files/ZDUIComponent/ZDUIComponent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZDUIComponent : NSObject
+@end
+@implementation PodsDummy_ZDUIComponent
+@end
---
+++ Example/Pods/Target Support Files/ZDUIComponent/ZDUIComponent-umbrella.h
@@ -0,0 +1,32 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BaseCollectionCell.h"
+#import "BaseCollectionView.h"
+#import "BaseNestTableView.h"
+#import "BaseTableView.h"
+#import "BaseTableViewCell.h"
+#import "BaseUIView.h"
+#import "BaseCollectionViewController.h"
+#import "BaseDetailViewController.h"
+#import "BaseListViewController.h"
+#import "BaseNavigationController.h"
+#import "BaseNewTableViewController.h"
+#import "BaseTabBarController.h"
+#import "BaseViewController.h"
+#import "FullLoadingView.h"
+#import "LoadingMaskView.h"
+#import "NoDataOrNetView.h"
+
+FOUNDATION_EXPORT double ZDUIComponentVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZDUIComponentVersionString[];
+
